b9fe26e239e06489938881929d4bfe2b6539ea93
Petrus Prinsloo <50941608+PPrinsloo@users.noreply.github.com>
2020-06-14 17:25:08 +0200

    Aligned a bit better with tutorial to use similar naming and not using vie "lookAt" function for now. (#4)

--- main.go
+++ main.go
@@ -38,13 +38,14 @@ func main() {
 	gl.UseProgram(program)
 
 	//Transformation Matrices
-	projection := mgl.Perspective(mgl.DegToRad(45.0), float32(cnf.Width)/float32(cnf.Height), 0.1, 10.0)
+	projection := mgl.Perspective(mgl.DegToRad(45.0), float32(cnf.Width)/float32(cnf.Height), 0.1, 100.0)
 	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
 	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
 
-	camera := mgl.LookAtV(mgl.Vec3{3, 3, 3}, mgl.Vec3{0, 0, 0}, mgl.Vec3{0, 1, 0})
-	cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
-	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
+	//view := mgl.LookAtV(mgl.Vec3{3, 3, 3}, mgl.Vec3{0, 0, 0}, mgl.Vec3{0, 1, 0})
+	view := mgl.Translate3D(0.0, 0.0, -3.0)
+	viewUniform := gl.GetUniformLocation(program, gl.Str("view\x00"))
+	gl.UniformMatrix4fv(viewUniform, 1, false, &view[0])
 
 	model := mgl.Ident4()
 	modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
@@ -70,13 +71,18 @@ func main() {
 // loop over cells and tell them to draw
 func draw(vao uint32, window *glfw.Window, program uint32, texture *uint32, model mgl.Mat4, modelUniform int32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-	time := glfw.GetTime()
-	elapsed := time - previousTime
-	previousTime = time
-
-	angle += elapsed
-	model = mgl.HomogRotate3D(float32(angle), mgl.Vec3{0, 1, 0})
+	cubePositions := []mgl.Vec3{
+		{0.0, 0.0, 0.0},
+		{2.0, 5.0, -15.0},
+		{-1.5, -2.2, -2.5},
+		{-3.8, -2.0, -12.3},
+		{2.4, -0.4, -3.5},
+		{-1.7, 3.0, -7.5},
+		{1.3, -2.0, -2.5},
+		{1.5, 2.0, -2.5},
+		{1.5, 0.2, -1.5},
+		{-1.3, 1.0, -1.5},
+	}
 
 	gl.UseProgram(program)
 
@@ -87,7 +93,13 @@ func draw(vao uint32, window *glfw.Window, program uint32, texture *uint32, mode
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindTexture(gl.TEXTURE_2D, *texture)
 
-	gl.DrawArrays(gl.TRIANGLES, 0, 6*2*3)
+	for i, cube := range cubePositions {
+		model = mgl.Ident4()
+		model = model.Mul4(mgl.Translate3D(cube.X(), cube.Y(), cube.Z()))
+		model.Mul4(mgl.HomogRotate3D(20.0*float32(i), mgl.Vec3{1, 0.3, 0.5}))
+		gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+		gl.DrawArrays(gl.TRIANGLES, 0, 36)
+	}
 
 	// Maintenance
 	window.SwapBuffers()
--- resources/shaders/vertex/shader.glsl
+++ resources/shaders/vertex/shader.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 
-uniform mat4 projection;
-uniform mat4 camera;
 uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 in vec3 vert;
 in vec2 vertTexCoord;
@@ -12,5 +12,5 @@ out vec2 fragTexCoord;
 void main()
 {
     fragTexCoord = vertTexCoord;
-    gl_Position = projection * camera * model * vec4(vert, 1);
+    gl_Position = projection * view * model * vec4(vert, 1);
 }
